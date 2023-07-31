ab1565e91d54d0963c2f21b1a99ab24b41d33192
Argumelar Pamungkas <53247359+argumpamungkas@users.noreply.github.com>
2023-07-31 10:46:12 +0700

    Time: 234 ms (98.40%), Space: 143 MB (64.71%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.dart
@@ -0,0 +1,22 @@
+class Solution {
+  int searchInsert(List<int> nums, int target) {
+      
+    if(nums.any((e) => e == target)){
+        int result = nums.indexWhere((e) => e == target);
+        return result;
+    } else {
+        for(var i = 0; i < nums.length; i++){
+            if(i < nums.length - 1 && (target > nums[i] && target < nums[i+1])){
+                return i + 1;
+            } else if (target > nums[i]){
+                if(i >= nums.length - 1){
+                    return i + 1;
+                } else {
+                    continue;
+            }
+        }
+        }
+    }
+      return 0;
+  }
+}
