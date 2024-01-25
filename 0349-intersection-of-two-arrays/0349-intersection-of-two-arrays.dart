041f95bc2bc764b17211fb26a26e13d774e2bde0
Argumelar Pamungkas <53247359+argumpamungkas@users.noreply.github.com>
2024-01-25 16:55:32 +0700

    Time: 362 ms (38.18%), Space: 149.7 MB (40.00%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.dart
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.dart
@@ -1,21 +1,15 @@
 class Solution {
   List<int> intersection(List<int> nums1, List<int> nums2) {
     List<int> result = [];
-  
-    for(var i = 0; i < nums1.length; i++){
-    if(nums2.any((e) => nums1[i] == e)){
-      if(result.isNotEmpty){
-        if(result.any((e) => e == nums1[i])){
-          continue;
-        } else {
-          result.add(nums1[i]);
-        }
-      } else {
-        result.add(nums1[i]);
+      
+      for (var i in nums1){
+          for(var j in nums2){
+              if(i == j){
+                  result.add(i);
+                  result = result.toSet().toList();
+              }
+          }
       }
-    }
-  }
-  
-  return result;
+      return result;
   }
 }
