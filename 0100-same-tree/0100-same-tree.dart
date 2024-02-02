6b421d23db8932673987bfd7b6ec1ea6c237d08a
Argumelar Pamungkas <53247359+argumpamungkas@users.noreply.github.com>
2024-02-02 14:50:45 +0700

    Time: 321 ms (35.21%), Space: 152.5 MB (7.04%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.dart
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   int val;
+ *   TreeNode? left;
+ *   TreeNode? right;
+ *   TreeNode([this.val = 0, this.left, this.right]);
+ * }
+ */
+class Solution {
+  bool isSameTree(TreeNode? p, TreeNode? q) {
+    if(p == null && q == null) return true;
+    
+      if(p == null || q == null || p.val != q.val) return false;
+      
+      return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    
+  }
+}
